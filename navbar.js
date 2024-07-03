7dda1b28c91322262c9980de1d13aa493498e0be
KG-dev128 <kirangull568@gmail.com>
2024-07-03 12:13:30 +0500

    logo me prblem solve krdi mene

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.getElementById("nav").innerHTML=`
 <nav>
         <div class="container">
             <a href="#">
-          <img src="../education-web/assets/images/logo.png" height="80px" width="80px"></a>
+          <img src="../education-web/assets/images/logo.jpg" height="80px" width="80px"></a>
             <div>
                 <a href="Home" class="links">Home  </a>
                 <a href="#" class="links">Service  </a>
