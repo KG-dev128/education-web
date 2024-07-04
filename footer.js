fd258dd06059d1146472cab6d93dafe421df2314
uzma672 <uzmauzma672@gmail.com>
2024-07-04 17:40:49 +0500

    xyz

--- footer.js
+++ footer.js
@@ -1,5 +1,7 @@
 document.getElementById("footer").innerHTML=`
-<footer class="bg-dark text-light text-center mt-3">
+<footer>
+<div class="foot-body">
                 <p>ALL RIGHT RESERVED BY APTECH 2024 &COPY;</p>
-              </footer>
+    </div>
+                </footer>
 `
--- navbar.js
+++ navbar.js
@@ -1,15 +1,15 @@
 document.getElementById("nav").innerHTML=`
-<nav>
-        <div class="container">
-            <a href="#">
-          <img src="../education-web/assets/images/logo.jpg" height="80px" width="80px"></a>
-            <div>
-                <a href="Home" class="links">Home  </a>
-                <a href="#" class="links">Service  </a>
-                <a href="#" class="links">About Us  </a>
-                <a href="#" class="links">Contact Us  </a>
-            </div>
-            <button>Register</button>
-        </div>
-    </nav>
+ <header>
+        
+     <img src="assets/images/logo.png" class="logo" >
+        <nav>
+            <ul>
+                <li class="active"><a href="">HOME</a></li>
+                <li><a href="">ABOUT US </a></li>
+                <li><a href="">COURSES</a></li>
+                <li><a href="">CONTACT US</a></li>
+            </ul>
+        </nav>
+        <a href=""><button>REGISTER </button></a>
+    </header>
     `
