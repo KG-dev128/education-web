c38d40fa9db0f05291cf218723661acf043fe12b
KG-dev128 <kirangull568@gmail.com>
2024-07-09 09:47:13 +0500

    footer completed

--- footer.js
+++ footer.js
@@ -1,1 +1,20 @@
-document.getElementById("footer").innerHTML=``
+document.getElementById("footer").innerHTML=`
+<footer>
+    <ul class="icons">
+        <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+        <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+        <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+        <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+    </ul>
+    <ul class="list">
+        <li><a href="home.html" target="_blank">Home</a></li>
+        <li><a href="" target="_blank">Services</a></li>
+        <li><a href="About US.html" target="_blank">About</a></li>
+        <li><a href="" target="_blank">Contact</a></li>
+        <li><a href="" target="_blank">Team</a></li>
+    <ul>
+    <div>
+    <p> © 2024 <a href="home.html">Excellent Coaching <a>| All Rights Reserved </p>
+    </div>
+</footer>
+`
