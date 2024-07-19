73bf4066f9f0f83a39a97a9538baf54e0abb309e
uzma672 <uzmauzma672@gmail.com>
2024-07-19 18:29:06 +0500

    halfdone

--- navbar.js
+++ navbar.js
@@ -1,18 +1,34 @@
-document.getElementById("nav").innerHTML=`
+document.getElementById("nav").innerHTML = `
  <header>
         
      <img src="assets/images/LOGO.png" class="logo" >
         <nav>
-            <ul>
+            <ul id="nav-list">
                 <li>
                 <a href="home.html" class="active">HOME</a>
                 </li>
-                <li><a href="">ABOUT US </a></li>
-                <li><a href="services.html">SERVICES</a></li>
-                <li><a href="">CONTACT US</a></li>
-            </ul>
-        </nav>
-       
-        <a href="" class="btn"><button>REGISTER</button></a>
+                <li><a href="About US.html">ABOUT US </a></li>
+                <div class="dropdown">
+                             <li><a href="cource.html" class="dropbtn">COURCES
+                             <i class="fa fa-caret-down"></i>
+                                    </a></li>
+                                    <div class="dropdown-content">
+                                    <a href="HDSE.html">HDSE</a>
+                                    <a href="ADSE.html">ADSE</a>
+                                    <a href="sc.html">SHORT COURSES</a>
+                                    <a href="lc.html">LANGUAGE COURSES</a>
+                                    </div>
+                                    </div>    
+                                    <li><a href="Contact Us.html">CONTACT US </a></li>
+                                    </ul>
+                                    </nav>
+                                    
+                                    <a href="registration.html" class="btn"><button>REGISTER</button></a>
+                                    <button hidden id="menu-btn" onclick="menushowandhide()"><img src="assets/images/menu-icon.webp" height="50" width="50" /></button>
     </header>
     `
+
+
+function menushowandhide() {
+    document.getElementById("nav-list").classList.toggle("active")
+}
