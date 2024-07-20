6bcdd03b7494b93f56079257d3d44300e34458f6
KG-dev128 <kirangull568@gmail.com>
2024-07-20 15:13:49 +0500

    Revert "data upload"
    This reverts commit b7d237e35fa4e648da95bd0650501c04090d93c3.

--- navbar.js
+++ navbar.js
@@ -1,40 +1,15 @@
-document.getElementById("nav").innerHTML = `
- <header>
-        
-     <img src="assets/images/LOGO.png" class="logo" >
-        <nav>
-            <ul id="nav-list">
-                <li>
-                <a href="home.html" class="active">HOME</a>
-                </li>
-                <li><a href="About US.html">ABOUT US </a></li>
-                <div class="dropdown">
-                             <li><a href="course.html" class="dropbtn">COURSES
-                             <i class="fa fa-caret-down"></i>
-                                    </a></li>
-                                    <div class="dropdown-content">
-                                    <a href="HDSE.html">HDSE</a>
-                                    <a href="ADSE.html">ADSE</a>
-                                    <a href="sc.html">SHORT COURSES</a>
-                                    <a href="lc.html">LANGUAGE COURSES</a>
-                                    </div>
-                                    </div>    
-                                    <li><a href="Contact Us.html">CONTACT US </a></li>
-                                    </ul>
-                                    </nav>
-                                    
-                                    <a href="registration.html" class="btn"><button>REGISTER</button></a>
-                                    <button hidden id="menu-btn" onclick="menushow()"><img src="assets/images/menu-icon.webp" height="50" width="50" /></button>
-                                    <button hidden id="close-menu-btn" onclick="menuhide()"><img src="assets/images/menu-icon.webp" height="50" width="50" /></button>
-
-                                    </header>
+document.getElementById("nav").innerHTML=`
+<nav>
+        <div class="container">
+            <a href="#">
+          <img src="../education-web/assets/images/logo.jpg" height="80px" width="80px"></a>
+            <div>
+                <a href="Home" class="links">Home  </a>
+                <a href="#" class="links">Service  </a>
+                <a href="#" class="links">About Us  </a>
+                <a href="#" class="links">Contact Us  </a>
+            </div>
+            <button>Register</button>
+        </div>
+    </nav>
     `
-
-
-function menushow() {
-    document.getElementById("nav-list").classList.add("active")
-}
-
-function menuhide() {
-    document.getElementById("nav-list").classList.remove("active")
-}
