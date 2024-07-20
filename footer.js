d0cfc8b314eef1ec7b55badb2f0fdbc806761932
uzma672 <uzmauzma672@gmail.com>
2024-07-20 12:22:17 +0500

    media

--- footer.js
+++ footer.js
@@ -1,7 +1,3 @@
 document.getElementById("footer").innerHTML=`
-<footer>
-<div class="foot-body">
-                <p>ALL RIGHT RESERVED BY APTECH 2024 &COPY;</p>
-    </div>
-                </footer>
+
 `
